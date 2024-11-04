56570816e15642add867e5b9baba6f57c6c4cd33
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2024-11-04 22:41:02 +0100

    vratil jsem gitignore

--- src/obj/Debug/net8.0/TourDeApp.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/TourDeApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourDeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c0fe751f005b803948fd23a0cc27bb3cc0cb7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3325d8cfd5fefa6a5d6bf287a350d48f0ab6bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourDeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourDeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
