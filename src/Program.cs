e75c6ead44441e6b39178590411676e2fed98c5f
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2024-11-05 16:58:40 +0100

    Build works, app does not

--- src/Program.cs
+++ src/Program.cs
@@ -1,20 +1,17 @@
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorComponents(); // Add Blazor services
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -24,8 +21,10 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapBlazorHub(); // Map Blazor Hub
+app.MapFallbackToPage("/_Host"); // Fallback to Blazor
 
 app.Run();
-
