dac4f5b0508c4bbd9530597b1b426eef2f730bb3
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2024-11-11 19:35:41 +0100

    Made Cell.blazor, does not work

---
+++ src/Components/Models/GameCreateUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace TourDeApp.Components.Models
+{
+    public class GameCreateUpdateRequest
+    {
+        private Game _game { get; set; }
+
+        public GameCreateUpdateRequest(Game game)
+        {
+            _game = game;
+        }
+    }
+}
