a29eb0b10bf8d4b06e4d7314db171c2c1be941ec
Elexic <89549464+elex1c@users.noreply.github.com>
2024-11-11 11:31:38 +0100

    Revert "Update #5"
    This reverts commit af3c3bc1b6f0c18c69d3632e7d6d3d497f4f7755.

--- src/Models/GameCreateUpdateRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TourDeApp.Models
-{
-    public class GameCreateUpdateRequest
-    {
-        public string Name { get; set; }
-        public DifficultyType Difficulty { get; set; }
-        public string[][] Board { get; set; }
-    }
-}
