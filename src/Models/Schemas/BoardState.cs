e167b52afc1242028d6e0cbb093594d764ad8c33
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2024-11-13 19:28:21 +0100

    Changed BoardState.cs back

--- src/Models/Schemas/BoardState.cs
+++ src/Models/Schemas/BoardState.cs
@@ -2,21 +2,12 @@
 {
     public class BoardState
     {
-        public string[,] Board { get; set; }
+        public Cell[,] Board { get; set; }
 
 
         public BoardState(int size = 15)
         {
-            Board = new string[size, size];
-
-            // Nastaví všechny hodnoty na nic (those who know)
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                {
-                    Board[i, j] = "";
-                }
-            }
+            Board = new Cell[size, size];
         }
     }
 }
