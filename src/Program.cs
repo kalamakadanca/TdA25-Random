eeba677351dd2e2db31512ca3bb8fa637400ca0b
elex1c <gift228338@gmail.com>
2024-11-16 20:57:14 +0200

    Quick

--- src/Program.cs
+++ src/Program.cs
@@ -2,12 +2,15 @@ using Microsoft.EntityFrameworkCore;
 using TourDeApp;
 using TourDeApp.Components;
 using TourDeApp.Components.Services;
+using TourDeApp.Models.Schemas;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DatabaseContext>(options =>
 {
@@ -26,6 +29,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
