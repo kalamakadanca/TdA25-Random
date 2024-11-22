a5644307efb686c4f5b6de113288bc197564a880
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2024-11-22 18:19:56 +0100

    Created class Account

---
+++ src/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿namespace TourDeApp.Models;
+
+public class Account
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    
+    // TODO: Store games created by account
+    // public Game[} Games { get; set; }
+
+    public Account(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+}
