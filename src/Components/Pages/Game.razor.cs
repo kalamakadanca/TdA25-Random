c84ea68464b4eab1ab0c081decf7c7461a045a1f
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2025-02-16 14:50:35 +0100

    Code readability improvements
    Moved code from Home.razor and Game.razor to Home.razor.cs and Game.razor.cs

---
+++ src/Components/Pages/Game.razor.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using TourDeApp.Components.Services;
+using TourDeApp.Controllers.API_V1.Games;
+using TourDeApp.Models;
+using TourDeApp.Models.Schemas;
+
+namespace TourDeApp.Components.Pages;
+
+public partial class Game : ComponentBase
+{
+    [Inject] private NavigationManager _navigationManager { get; set; } = default!;
+    [Inject] private GameService _gameService { get; set; } = default!;
+    [Inject] private GamesController _gamesController { get; set; } = default!;
+
+    
+    public int _difficulty = 0;
+    List<DifficultyType> obt = new List<DifficultyType>();
+    
+    public bool hr_ed = true;
+
+    private string? _gameUuid { get; set; }
+    private string? _saveName { get; set; }
+    private DifficultyType _saveDifficulty { get; set; }
+    private Modal? ModalRef;
+    private Modal WinningDialog;
+    private UserModel UserModel { get; set; } = new();
+    public bool meh = true;
+    private Models.Game defaultGame = new Models.Game("Default game", DifficultyType.Beginner);
+    private bool gamesLoaded = false;
+    private List<Models.Game>? games { get; set; }
+
+    private void NewGame()
+    {
+        if (_navigationManager.Uri.Contains("/game/"))
+        {
+            _navigationManager.NavigateTo("/game", true);
+        }
+        else
+        {
+            _game = _gameService.CreateGame();
+            StateHasChanged();
+        }
+    }
+
+    private Models.Game? _game { get; set; }
+    [Parameter] public string? Uuid { get; set; }
+
+    public void ChangeDifficulty(DifficultyType difficulty)
+    {
+        _saveDifficulty = difficulty;
+        Console.WriteLine(_saveDifficulty);
+        StateHasChanged();
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        string currentUrl = _navigationManager.Uri;
+        if (currentUrl.Contains("/game/"))
+        {
+            var index = currentUrl.IndexOf("/game/") + "/game/".Length;
+            Uuid = currentUrl.Substring(index);
+            _game = await LoadGame(Uuid);
+        }
+        else
+        {
+            _game = _gameService.CreateGame();
+        }
+    }
+
+    private async Task SaveGame()
+    {
+        if (_game == null || _saveName == null || _saveDifficulty == null) return;
+        if (!string.IsNullOrEmpty(_saveName)) _game.Name = _saveName;
+        if (_saveDifficulty != default) _game.Difficulty = _saveDifficulty;
+        var request = new GameCreateUpdateRequest(new Models.Game
+        {
+            Name = _saveName,
+            Difficulty = _saveDifficulty,
+            Board = _game.Board
+        });
+        var result = await _gamesController.Post(request);
+
+        if (result is ObjectResult objectResult && objectResult.StatusCode == 201)
+        {
+            if (objectResult.Value is Models.Game game)
+            {
+                Uuid = game.Uuid;
+                _navigationManager.NavigateTo($"/game/{Uuid}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Hra se NEuložila :(");
+        }
+    }
+
+    private async Task<Models.Game> LoadGame(string uuid)
+    {
+        if (uuid == null) return defaultGame;
+
+        var result = await _gamesController.Get(uuid);
+
+        if (result is ObjectResult objectResult && objectResult.StatusCode == 200)
+        {
+            if (objectResult.Value is TourDeApp.Models.Game game)
+            {
+                return game;
+            }
+            return defaultGame;
+        }
+
+        Console.WriteLine("There has been an error");
+        return defaultGame;
+    }
+
+
+    private async Task HandleClick()
+    {
+        OpenModal();
+        await SaveGame();
+    }
+
+    private void OpenModal() => ModalRef?.Open();
+    private void HandleValidSubmit()
+    {
+        ModalRef?.Close();
+    }
+
+    private void GameWon()
+    {
+        
+    }
+}
---
+++ src/Components/Pages/Home.razor.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using TourDeApp.Components.Services;
+using TourDeApp.Controllers.API_V1.Games;
+using TourDeApp.Models;
+using TourDeApp.Models.Schemas;
+
+namespace TourDeApp.Components.Pages;
+
+public partial class Home : ComponentBase
+{
+    [Inject] private NavigationManager _navigationManager { get; set; } = default!;
+    [Inject] private GamesController _gamesController { get; set; } = default!;
+    
+    
+    private Modal? ModalRef;
+    private Modal multiplayerSelectModal;
+    private UserModel UserModel { get; set; } = new();
+    public bool meh = true;
+    private Models.Game defaultGame = new Models.Game("Default game", DifficultyType.Beginner);
+    private bool gamesLoading { get; set; }
+    private List<Models.Game>? _games;
+    public List<Models.Game>? Games
+    {
+        get
+        {
+            return _games;
+        }
+        set
+        {
+            _games = value;
+            StateHasChanged();
+        }
+    }
+
+    private void OpenModal() => ModalRef?.Open();
+    private void HandleValidSubmit()
+    {
+        ModalRef?.Close();
+    }
+    private void PlayLocally()
+    {
+        _navigationManager.NavigateTo("/game", true);
+    }
+
+    private void PlayOnline()
+    {
+        multiplayerSelectModal.Open();
+    }
+    
+    private IList<Todoitem> Todos { get; set; } = new List<Todoitem>();
+
+    protected override void OnInitialized()
+    {
+        gamesLoading = true;
+        // Start a background task to load games without blocking the UI
+        _ = LoadGamesAsync();
+    }
+
+    private async Task LoadGamesAsync()
+    {
+        Games = await GetGames();
+        gamesLoading = false;
+        await InvokeAsync(StateHasChanged); // Notify Blazor to update the UI
+    }
+
+    public string ItemClass(Todoitem item)
+    {
+        return item.Completed ? "item-completed" : "";
+    }
+
+    private async Task<List<Models.Game>> GetGames()
+    {
+        var result = await _gamesController.Get();
+
+        if (result is ObjectResult objectResult && objectResult.StatusCode == 200)
+        {
+            if (objectResult.Value is List<Models.Game> games)
+            {
+                return games;
+            }
+        }
+
+        return new List<Models.Game>() { defaultGame };
+    }
+
+    private void OpenGame(string uuid)
+    {
+        _navigationManager.NavigateTo($"/game/{uuid}");
+    }
+}
